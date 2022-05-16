c43338e99367d089cbab21a71abfc877dcf21893
unyuu <unyuu@tenshi.nekomimi.nya>
2022-05-16 14:28:34 +0500

    Самый первый вариант

---
+++ 20220516-Test-Project-1/main.cc
@@ -0,0 +1,18 @@
+/*
+ * main.cc
+ *
+ *  Created on: 16 мая 2022 г.
+ *      Author: unyuu
+ */
+
+#include <iostream>
+
+
+int main(int, char **)
+{
+	std::cout << "This is a test project!" << std::endl;
+
+	return 0;
+}
+
+
