056559947e8a1fefeb16a3b4e7b94fbe503fb592
unyuu <unyuu@tenshi.nekomimi.nya>
2022-05-16 14:33:17 +0500

    Новая экспериментальная фишка!

--- 20220516-Test-Project-1/main.cc
+++ 20220516-Test-Project-1/main.cc
@@ -6,11 +6,15 @@
  */
 
 #include <iostream>
-
+#include <string>
 
 int main(int, char **)
 {
 	std::cout << "Это тестовый проект!" << std::endl;
+	std::string name;
+	std::cout << "Как Вас зовут? ";
+	std::getline(std::cin, name);
+	std::cout << "Привет, " << name << "!\n";
 
 	return 0;
 }
